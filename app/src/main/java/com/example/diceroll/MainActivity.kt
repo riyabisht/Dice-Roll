40399f446816fb9b4ff0fc54e639dcd77de4207e
riyabisht <riyabisht152@gmail.com>
2022-07-19 12:39:07 +0530

    dice app

---
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.diceroll
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
+        val dice = Dice(6)
+        val rollDiceResult = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage : ImageView = findViewById(R.id.imageView2)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableImage = when(rollDiceResult){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else-> {
+                R.drawable.dice_1
+            }
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableImage)
+
+        // Update the content description
+        diceImage.contentDescription = rollDiceResult.toString()
+    }
+}
+class Dice(private val numSides : Int){
+    fun roll():Int{
+        return (1..numSides).random()
+    }
+}
